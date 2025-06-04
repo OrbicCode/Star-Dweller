7835b4059265992618d8265202bd2115cc1a6625
James O'Kane <orbiccode@gmail.com>
2025-06-04 16:42:49 +0100

    added mock for useRouter push

--- cosmic-dashboard/src/app/auth/signup/tests/signup.test.tsx
+++ cosmic-dashboard/src/app/auth/signup/tests/signup.test.tsx
@@ -1,10 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import SignUp from "../page";
 
+jest.mock("next/navigation", () => ({
+	useRouter: () => ({
+		push: jest.fn(),
+	}),
+}));
+
 describe("Sign Up Page", () => {
 	it("renders the signup form", () => {
 		render(<SignUp />);
 		expect(screen.getByRole("form")).toBeInTheDocument();
 		expect(screen.getByLabelText("Email:")).toBeInTheDocument();
+		expect(screen.getByLabelText("Password:")).toBeInTheDocument();
 	});
 });
