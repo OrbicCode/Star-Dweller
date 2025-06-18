5439d06f94ef0e318606e2c42213ff8bd310e9da
James O'Kane <orbiccode@gmail.com>
2025-06-18 15:56:43 +0100

    true true

--- src/components/auth/AuthModal/AuthModal.test.tsx
+++
@@ -1,2 +0,0 @@
-// jest.mock('@/components/LoginForm/LoginForm', () => jest.fn(() => <div>Login Form</div>));
-// jest.mock('@/components/SignupForm/SignupForm', () => jest.fn(() => <div>Signup Form</div>));
