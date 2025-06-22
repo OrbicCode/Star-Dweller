f6400293a0aa70c486f108ba0524930313c7a179
James O'Kane <orbiccode@gmail.com>
2025-06-22 15:13:10 +0100

    widget card background

--- src/components/WidgetCard/WidgetCard.test.tsx
+++ src/components/WidgetCard/WidgetCard.test.tsx
@@ -1,11 +1,11 @@
-// import { render, screen } from '@testing-library/react';
-// import WidgetCard from './WidgetCard';
+import { render, screen } from '@testing-library/react';
+import WidgetCard from './WidgetCard';
 
-// it('should render title', () => {
-//   render(
-//     <WidgetCard title='To-Do'>
-//       <></>
-//     </WidgetCard>
-//   );
-//   expect(screen.getByText('To-Do'));
-// });
+it('should render title', () => {
+  render(
+    <WidgetCard title='To-Do' background={null}>
+      <></>
+    </WidgetCard>
+  );
+  expect(screen.getByText('To-Do'));
+});
