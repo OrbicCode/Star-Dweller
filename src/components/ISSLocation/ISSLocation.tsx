f717a56e2a46ad9b17df1cbb6425640d4e15298e
James O'Kane <orbiccode@gmail.com>
2025-06-24 17:02:21 +0100

    map recenter function

--- src/components/ISSLocation/ISSLocation.tsx
+++ src/components/ISSLocation/ISSLocation.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import styles from './ISSLocation.module.css';
 
 export default function ISSLocation() {
@@ -29,14 +29,23 @@ export default function ISSLocation() {
       }
     }
     fetchLocation();
-    const interval = setInterval(fetchLocation, 30000);
+    const interval = setInterval(fetchLocation, 60000);
     return () => clearInterval(interval);
   }, []);
+
+  const RecenterMap = ({ lat, lon }: { lat: number; lon: number }) => {
+    const map = useMap();
+    useEffect(() => {
+      map.setView([lat, lon], 0);
+    }, [lat, lon, map]);
+    return null;
+  };
+
   return (
     <div className={styles.container}>
       <MapContainer
         center={location ? [location.lat, location.lon] : [51.505, -0.09]}
-        zoom={3}
+        zoom={0}
         scrollWheelZoom={false}
         style={{ height: '100%', width: '100%' }} // Ensure the map fills the container
       >
@@ -45,13 +54,12 @@ export default function ISSLocation() {
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
         {location && (
-          <Marker position={[location.lat, location.lon]}>
-            <Popup>
-              ISS Current Location
-              <br />
-              Lat: {location.lat.toFixed(2)}, Lon: {location.lon.toFixed(2)}
-            </Popup>
-          </Marker>
+          <>
+            <Marker position={[location.lat, location.lon]}>
+              <Popup>ISS Current Location</Popup>
+            </Marker>
+            <RecenterMap lat={location.lat} lon={location.lon} />
+          </>
         )}
       </MapContainer>
     </div>
