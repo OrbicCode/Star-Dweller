7b38f983ed5e26aaeaec1bfe6b5be59c67d1770a
James O'Kane <orbiccode@gmail.com>
2025-07-03 23:34:06 +0100

    api route change

--- src/app/api/iss_location/route.ts
+++ src/app/api/iss_location/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    const response = await fetch('https://api.open-notify.org/iss-now.json');
+    const response = await fetch('http://api.open-notify.org/iss-now.json');
     if (!response.ok) {
       return NextResponse.json(
         { error: 'Failed to fetch ISS location' },
