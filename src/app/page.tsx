1c7a3c1de0ad8dc637be2e9dfa2bc9915ee93534
James O'Kane <orbiccode@gmail.com>
2025-07-06 20:05:24 +0100

    responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+'use client';
+import { useState } from 'react';
 import Image from 'next/image';
+import AuthModal from '@/components/auth/AuthModal/AuthModal';
 import styles from './page.module.css';
 
 export default function Home() {
+  const [isAuthModalOpen, setIsAuthModalOpen] = useState<boolean>(false);
+
   return (
     <div className={styles.container}>
       <div className={styles.hero}>
@@ -11,6 +16,12 @@ export default function Home() {
           Manage your tasks, keep up with space news, see when the next SpaceX
           launch is and more
         </p>
+        <button
+          onClick={() => setIsAuthModalOpen(true)}
+          className={styles.getStartedBtn}
+        >
+          Get Started
+        </button>
       </div>
       <div className={styles.imageSection}>
         <div className={styles.dashboardImageContainer}>
@@ -22,13 +33,76 @@ export default function Home() {
           />
         </div>
       </div>
-      <div>
-        <div>
-          <span className={`material-symbols-outlined burger-menu`}>menu</span>
-          <h3>To-Do List</h3>
-          <p>manage your tasks with a to-do list saved to your account</p>
+      <section className={styles.featureSection}>
+        <h2>Stay productive in the stars</h2>
+        <div className={styles.featuresContainer}>
+          <div className={styles.feature}>
+            <span className={`material-symbols-outlined burger-menu`}>
+              checklist
+            </span>
+            <h3>To-Do List</h3>
+            <p className={styles.featureInfo}>
+              Manage your tasks with a to-do list saved to your account
+            </p>
+          </div>
+
+          <div className={styles.feature}>
+            <span className={`material-symbols-outlined burger-menu`}>
+              breaking_news
+            </span>
+            <h3>Space News</h3>
+            <p className={styles.featureInfo}>
+              Get the latest news on everything related to space!
+            </p>
+          </div>
+
+          <div className={styles.feature}>
+            <span className={`material-symbols-outlined burger-menu`}>
+              rocket_launch
+            </span>
+            <h3>Next SpaceX Launch</h3>
+            <p className={styles.featureInfo}>
+              Want to know when the next SpaceX launch is, we&#39;ve got you
+              covered
+            </p>
+          </div>
+
+          <div className={styles.feature}>
+            <span className={`material-symbols-outlined burger-menu`}>
+              satellite_alt
+            </span>
+            <h3>ISS Location</h3>
+            <p className={styles.featureInfo}>
+              Get the live location of the ISS so you know when to look up
+            </p>
+          </div>
+
+          <div className={styles.feature}>
+            <span className={`material-symbols-outlined burger-menu`}>
+              orbit
+            </span>
+            <h3>Who&#39;s in Space?</h3>
+            <p className={styles.featureInfo}>
+              See how many astronauts are in space, what their names are and
+              what craft they are in
+            </p>
+          </div>
+
+          <div className={styles.feature}>
+            <span className={`material-symbols-outlined burger-menu`}>
+              cloud_alert
+            </span>
+            <h3>Weather</h3>
+            <p className={styles.featureInfo}>
+              Grab the current weather conditions in your area. Currently set to
+              London, will have local location functionality coming soon.
+            </p>
+          </div>
         </div>
-      </div>
+      </section>
+      {isAuthModalOpen && (
+        <AuthModal onClose={() => setIsAuthModalOpen(false)} />
+      )}
     </div>
   );
 }
